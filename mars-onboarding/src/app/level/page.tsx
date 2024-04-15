aaded8b12ed9e66f9627cd35626114b4895d6744
jocelyn <jocelynz4890@gmail.com>
2024-04-15 16:19:30 -0400

    style levels page

--- mars-onboarding/src/app/level/page.tsx
+++ mars-onboarding/src/app/level/page.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 
 export default function LevelSelectPage() {
-    return <div>
-        <h1>Select your level:</h1>
-        <h2><Link href = "/level/git">Git Level</Link></h2>
-        <h2><Link href = "/level/linux">Linux Level</Link></h2>
-        <h2><Link href = "/level/ros">ROS Level</Link></h2>
-
-    </div>;
-
+    return (
+    <div>
+        <div className = "flex items-start content-start bg-pink-900 sticky top w-full h-30">
+            <Link href = "/" className = "text-white m-5 text-5xl hover:text-red-700">⬅</Link>
+        </div>
+        <div className = "flex-col min-h-screen w-full bg-black text-white">
+            <div className ="text-8xl select-none text-black bg-black">empty</div>
+            <h1 className = "m-5 bg-black select-none bottom-10 flex content-center items-center justify-center text-red-600 font-semibold text-5xl">Select your level:</h1>
+            <h2 className = "m-5 select-none flex content-center items-center justify-center hover:text-red-600 hover:animate-pulse hover:italic text-3xl hover:text-4xl"><Link href = "/level/git">Git Level</Link></h2>
+            <h2 className = "m-5 select-none flex content-center items-center justify-center hover:text-red-600 hover:animate-pulse hover:italic text-3xl hover:text-4xl"><Link href = "/level/linux">Linux Level</Link></h2>
+            <h2 className = "m-5 select-none flex content-center items-center justify-center hover:text-red-600 hover:animate-pulse hover:italic text-3xl hover:text-4xl"><Link href = "/level/ros">ROS Level</Link></h2>
+        </div>;
+    </div>
+    )
 }
