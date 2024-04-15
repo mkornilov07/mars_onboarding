1fe599858a765fc075a40a063e3ad739688f1cd9
jocelyn <jocelynz4890@gmail.com>
2024-04-15 15:01:50 -0400

    FINISHED HOME PAGE AAAAAA

--- mars-onboarding/tailwind.config.ts
+++ mars-onboarding/tailwind.config.ts
@@ -13,8 +13,56 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
+      height: {
+        '270': '270px',
+        '170':'170px',
+        '22%':"22%",
+        '500': '500px',
+        '300': '300px',
+        '90%':'90%',
+        '75%':'75%',
+        '70%':'70%',
+      },
+      width: {
+        '500': '500px',
+        '300': '300px',
+        '45%':"45%",
+        '90%':'90%',
+      },
+      animation: {
+        // steps(n) where n is the number of chars in the homepage <h1> tag
+        typewriter: 'typewriter 2s steps(52) forwards',
+        caret: 'typewriter 2s steps(52) forwards, blink 1s steps(52) infinite 1s',
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: '100%',
+          },
+        },
+        blink: {
+          '0%': {
+            opacity: '0',
+          },
+          '0.1%': {
+            opacity: '1',
+          },
+          '49%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: '0',
+          },
+          '50.1%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+      },
+    }
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animatecss')],
 };
 export default config;
