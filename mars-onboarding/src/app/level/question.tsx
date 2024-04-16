2ebd90e8409d5e2f3ff3a0215d15170a800f61ee
Mikhail Kornilov <beeboisgr8@gmail.com>
2024-04-15 21:46:21 -0400

    no

--- mars-onboarding/src/app/level/question.tsx
+++ mars-onboarding/src/app/level/question.tsx
@@ -1,38 +1,37 @@
-
-import React from 'react';
-import Editor from 'react-simple-code-editor';
-import { highlight, languages } from 'prismjs/components/prism-core';
+import { ReactDOM, createElement } from 'react';
+import React, { JSXElementConstructor } from 'react';
+import 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-python';
 import 'prismjs/themes/prism.css';
-export default function Question({children, starterCode, language, validate} : {children : React.ReactNode, starterCode : string, language : string, validate: (code : string) => boolean}) {
-  const [code, setCode] = React.useState(
-    starterCode
-  );
+import { Container } from 'postcss';
+export default function Question({children, starterCode, language, correctAnswers} : {children : React.ReactNode, starterCode : string, language : string, correctAnswers: string[]}) {
+  function handleBlanks(s : string) {
+    var i = 0;
+    var counter = 0;
+    var output = document.createElement('code');
+        while (i < s.length-5) {
+      if (s.substring(i, i+5) == "BLANK") {
+        output.innerHTML += `<input id = 'blank${counter}'></input>`
+        i += 5;
+      }
+      else {
+        output.innerHTML += s[i];
+        i += 1;
+      }
+    }
+    return output;
+  }
   function onSubmit() {
-    let submittedCode = code;
-    if (validate(submittedCode)) {
+    if (true) {
       console.log("Good");
     }
     else {
       console.log("Bad");
     }
   }
-    return (<div>
-      <code>
-        print("hello world")<br></br>
-        print("Hello <input></input>")<br/>
-        print("dog")<br/>
-      </code>
-        <Editor
-        value={code}
-        onValueChange={code => setCode(code)}
-        highlight={code => highlight(code, languages.python)}
-        padding={10}
-        style={{
-          fontFamily: '"Fira code", "Fira Mono", monospace',
-          fontSize: 12,
-        }}/>
+    return (
+      <Container child = {handleBlanks(starterCode)}/>
         
-    </div>)
+    )
 }
--- mars-onboarding/src/app/level/ros/page.tsx
+++ mars-onboarding/src/app/level/ros/page.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import Question from '../question';
 export default function ROSLevel({children,} : { children: React.ReactNode}) {
     return (<div><h1>ROS Level</h1>
-    <Question language="python" starterCode='dog' validate={(code) => true}><div></div></Question>
+    <Question language="python" starterCode='dogBLANK cat' correctAnswers={["correct"]}><div></div></Question>
     compoennt</div>);
 }
