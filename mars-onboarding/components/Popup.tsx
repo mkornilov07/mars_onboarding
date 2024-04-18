ca1611f326a5f5d5453a6754efb01efe56f3aeff
jocelyn <jocelynz4890@gmail.com>
2024-04-18 00:10:27 -0400

    longer popup text to show scroll

--- mars-onboarding/components/Popup.tsx
+++ mars-onboarding/components/Popup.tsx
@@ -52,6 +52,27 @@ export default function Popup() {
                     Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur 
                     proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
                 </p>
+                <p>
+                    Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
+                    dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. 
+                    Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. 
+                    Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur 
+                    proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
+                </p>
+                <p>
+                    Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
+                    dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. 
+                    Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. 
+                    Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur 
+                    proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
+                </p>
+                <p>
+                    Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
+                    dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. 
+                    Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. 
+                    Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur 
+                    proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
+                </p>
                 </ModalBody>
                 <ModalFooter>
                 <Button color="primary" variant="light" onPress={onClose}>
