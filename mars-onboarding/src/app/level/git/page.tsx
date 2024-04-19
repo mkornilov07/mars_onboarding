dcc6ca69a4cfe182bceb07da8d93f31a8b3918f4
Mikhail Kornilov <beeboisgr8@gmail.com>
2024-04-19 10:11:04 -0400

    fixed input

--- mars-onboarding/src/app/level/git/page.tsx
+++ mars-onboarding/src/app/level/git/page.tsx
@@ -4,7 +4,8 @@ import Level from "../../../../components/Level";
 
 export default function GitLevel() {
     let lesson = (<p>You'll need this</p>);
-    let title = (<code className="bg-transparent">git reset --hard</code>)
+    let title = (<code className="bg-transparent">git reset --hard</code>);
+    let starterCode = "%Oh no! You accidentally messed up the codebase. What do you do? \n BLANK";
     return (
-        <Level lesson = {lesson} title = {title} starterCode='' language='python' correctAnswers={[""]} levelName='Git Level'></Level>
+        <Level lesson = {lesson} title = {title} starterCode={starterCode} language='python' correctAnswers={["git reset --hard"]} levelName='Git Level'></Level>
 )};
--- mars-onboarding/src/app/level/question.tsx
+++ mars-onboarding/src/app/level/question.tsx
@@ -1,13 +1,18 @@
 'use client'
-import { ReactDOM, createElement, useEffect } from 'react';
+import { ReactDOM, createElement, useEffect, useState } from 'react';
 import React, { JSXElementConstructor } from 'react';
 import 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-python';
 import 'prismjs/themes/prism.css';
 import Prism from 'prismjs';
+import { FireworksInstance, fireworks } from '@tsparticles/fireworks';
+import Particles from '@tsparticles/react';
 export default function Question({children, starterCode, language, correctAnswers} : {children : React.ReactNode, starterCode : string, language : string, correctAnswers: string[]}) {
+  const [questionComplete, setQuestionComplete] = useState(false);
   function replaceBlanks(s : string, language : string) {
+    s = s.replaceAll('\n', '<br>');
+    // console.log(`Replacing BLANK in ${s}`);
     const BLANK = "BLANK";
     const LEN_BLANK=BLANK.length;
     let i = 0;
@@ -15,11 +20,12 @@ export default function Question({children, starterCode, language, correctAnswer
     let openingTag = `<code class = "language-${language} add-some-styling-later-idk-what-tailwind-looks-like">`;
     let closingTag = `</code>`;
     let output = openingTag;
-    while (i < s.length - LEN_BLANK) {
+    while (i < s.length - LEN_BLANK+1) {
+      // console.log(`Output is ${output}`);
       if (s.substring(i, i+LEN_BLANK) == BLANK) {
         output += closingTag + 
-        `<code><input autocomplete='false' id = 'blank${counter}' class = "text-black bg-gray-600 focus:bg-gray-300 focus:border-red-600"></input></code>` 
-        + openingTag;
+        `<input autocomplete='off' id = 'blank${counter}' class = "font-mono text-black bg-gray-600 focus:bg-gray-300 focus:border-red-600"></input>`;
+        if (i != s.length - LEN_BLANK) output += openingTag;
         counter += 1;
         i += LEN_BLANK;
       }
@@ -28,11 +34,12 @@ export default function Question({children, starterCode, language, correctAnswer
         i += 1;
       }
     }
-    output += s.substring(i, s.length);
+    output += s.substring(i+1, s.length);
     output += closingTag;
     return output;
   }
   function onSubmit() {
+    let allCorrect = true;
     for (let i = 0; i < correctAnswers.length; i++) {
       let box = document.getElementById(`blank${i}`);
       let response = box?.value;
@@ -44,14 +51,21 @@ export default function Question({children, starterCode, language, correctAnswer
       }
       else {
         console.log("Bad");
+        allCorrect = false;
         box?.animate([{"color": "red"}, {"color": "black"}], 2000);
       }
+      
+    }
+    if(allCorrect) {
+      fireworks({sounds:false});
+      setQuestionComplete(true);
     }
   }
     return (
       (<div>
         <div dangerouslySetInnerHTML={{__html: replaceBlanks(starterCode, language)}}></div>
         <br/>
+        <Particles></Particles>
         <button className ="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick = {onSubmit}>
           Submit
         </button>
