94f8434b4176c04d8793d03102f4a75388579e0c
jocelyn <jocelynz4890@gmail.com>
2024-07-30 21:00:59 -0700

    bab is sad want cweamy cweam

--- mars-onboarding/components/Level.tsx
+++ mars-onboarding/components/Level.tsx
@@ -27,19 +27,16 @@ data: allQuestionData, section : string, suffix : string, language : string, sol
     const titles = data[section].map(question => question.title);
     const checkFuncs = data[section].map(question => question.checkFunction);
     return (
-        <div>
-            <div className = "flex opacity-90 shadow-md shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f] shadow-black text-white bg-black sticky justify-evenly top w-full h-30">
+        <>
+            <div className = "h-32 flex opacity-90 shadow-md shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f] shadow-black text-white bg-black sticky justify-evenly top w-full h-30">
                 <Link href = "/level/" className = "text-white opacity-60 hover:opacity-100 m-5 self-center content-start select-none text-5xl hover:text-red-700">⬅</Link>
                 <div className = "flex-row m-6">
-                <h1 className = "m-10 select-none opacity-90 font-bold font-mono text-4xl place-self-center">
-                    {data[section][levelIndex].title} ({suffix} {levelIndex+1})
-                </h1>
+                <LevelSelector solvedQuestions = {solvedQuestions} section = {section} titles = {titles} setLevelIndex={setLevelIndex} setSubmit = {setSubmit} submitted = {submitted} questionId = {levelIndex} suffix = {suffix}></LevelSelector>
                 <div className = "m-10 place-content-center">
                     <Popup contents = {data[section][levelIndex].lesson} title = {data[section][levelIndex].title}></Popup>
                 </div>
                 </div>
                 <User logout = {logout} validateReq={validateReq}/>
-                <LevelSelector solvedQuestions = {solvedQuestions} section = {section} titles = {titles} setLevelIndex={setLevelIndex} setSubmit = {setSubmit} submitted = {submitted} questionId = {levelIndex}></LevelSelector>
             </div>
             <div className = "z-0 flex-col select-none min-h-screen w-full bg-zinc-900 text-white">
                 
@@ -50,7 +47,7 @@ data: allQuestionData, section : string, suffix : string, language : string, sol
                     <LevelBody numQuestions= {numQuestions} goToNextLevel = {goToNextLevel} levelIndex={levelIndex} language = {language} sectionData={data[section]} submitFunc={submitFunc} validateReq = {validateReq} section={section} questionIndex={levelIndex} setSubmit={setSubmit} checkFuncs = {checkFuncs}/>
                 </div>
             </div>
-        </div>);
+        </>);
 }
 
 export interface questionData {
--- mars-onboarding/components/LevelSelector.tsx
+++ mars-onboarding/components/LevelSelector.tsx
@@ -1,11 +1,14 @@
 'use client'
 import React, { useEffect, useState } from "react";
-import Dropdown from "rsuite/Dropdown";
+import {Dropdown, ButtonToolbar} from "rsuite";
 import {promises as fs} from 'fs';
 import Link from "next/link";
 import { Button } from "@nextui-org/react";
+import { ReactDOM } from "react";
 import { fetchSolvedQuestions } from "./LevelWrapper";
-export default function LevelSelector({titles, solvedQuestions, setLevelIndex, setSubmit, submitted, section, questionId} : {questionId : number, section : string, setSubmit: any, submitted: boolean, setLevelIndex: (i : number) => void, titles: Array<React.JSX.Element>, solvedQuestions: Array<number>}) {
+import "rsuite/dist/rsuite.css";
+
+export default function LevelSelector({titles, solvedQuestions, setLevelIndex, setSubmit, submitted, section, questionId, suffix} : {questionId : number, section : string, setSubmit: any, submitted: boolean, setLevelIndex: (i : number) => void, titles: Array<React.JSX.Element>, solvedQuestions: Array<number>, suffix: string}) {
     const [solvedQuestionsState, setSolvedQuestionsState] = useState([] as number[]);
     useEffect(()=>{setSolvedQuestionsState(solvedQuestions)}, [solvedQuestions]);
     useEffect(()=>{
@@ -21,6 +24,11 @@ export default function LevelSelector({titles, solvedQuestions, setLevelIndex, s
         <><text className={(i == questionId) ? "italic text-red-600" : ""}>{i+1}. {title}{solvedQuestionsState.includes(i) ? <text className = "text-lime-500">✓</text> : ""}</text></>
     )
     let itemList : Array<React.JSX.Element> = formattedTitles.map((title, i) =>
-    <Dropdown.Item key= {i} onSelect = {()=>setLevelIndex(i)}>{title}</Dropdown.Item>)
-    return <Dropdown title = "Levels">{itemList}</Dropdown>
+    <Dropdown.Item className = "text-lg p-9" key= {i} onSelect = {()=>setLevelIndex(i)}>{title}</Dropdown.Item>)
+    return <Dropdown style={{padding: 0, margin: 0}} title = 
+            {<h1 className = "m-10 select-none opacity-90 font-bold font-mono text-4xl place-self-center">
+                {titles[questionId]} ({suffix} {questionId+1})
+            </h1>}
+            className = "rs-theme-dark">{itemList}
+        </Dropdown>
     }
