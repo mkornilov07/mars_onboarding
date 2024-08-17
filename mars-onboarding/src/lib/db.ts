29ab1702f8a43bd7e99bba390693e1b53f4854ba
Mikhail <mkornilov07@gmail.com>
2024-08-17 00:27:54 -0700

    bug fix

--- mars-onboarding/src/lib/db.ts
+++ mars-onboarding/src/lib/db.ts
@@ -7,10 +7,10 @@ db("CREATE TABLE IF NOT EXISTS users(id TEXT NOT NULL PRIMARY KEY, picture TEXT)
 db(`CREATE TABLE IF NOT EXISTS session (
     id TEXT PRIMARY KEY,
     expires_at TIMESTAMPTZ NOT NULL,
-    user_id TEXT NOT NULL REFERENCES auth_user(id)
+    user_id TEXT NOT NULL REFERENCES users(id)
 )`);
 db(`CREATE TABLE IF NOT EXISTS solves (
     userId TEXT NOT NULL,
     questionIndex INTEGER NOT NULL,
     category TEXT NOT NULL,
-    FOREIGN KEY (userId) REFERENCES Users(id))`)
+    FOREIGN KEY (userId) REFERENCES users(id))`)
