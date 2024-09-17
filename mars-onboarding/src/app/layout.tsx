c36259f898406dcaec632ffb945cdebe7f3b5364
Mikhail <mkornilov07@gmail.com>
2024-09-17 10:10:38 -0400

    analytics

--- mars-onboarding/src/app/layout.tsx
+++ mars-onboarding/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-
+import { Analytics } from "@vercel/analytics/react"
 const roboto = Roboto({ subsets: ["latin"], weight: ["300"] });
 
 export const metadata: Metadata = {
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({children}: { children: React.ReactNode }){
-  return (
+  return (<Analytics>
     <html lang="en" className = "dark text-foreground bg-background select-none">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -25,5 +25,6 @@ export default function RootLayout({children}: { children: React.ReactNode }){
         </Providers>
       </body>
     </html>
+    </Analytics>
   );
 };
