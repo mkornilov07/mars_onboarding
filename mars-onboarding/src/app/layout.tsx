a14c47459a07f22aeccadb0e0d74ef94dac45df8
Mikhail <mkornilov07@gmail.com>
2024-09-17 10:19:06 -0400

    correct analytics

--- mars-onboarding/src/app/layout.tsx
+++ mars-onboarding/src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({children}: { children: React.ReactNode }){
-  return (<Analytics>
+  return (
     <html lang="en" className = "dark text-foreground bg-background select-none">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -23,8 +23,8 @@ export default function RootLayout({children}: { children: React.ReactNode }){
         <Providers>
           { children }
         </Providers>
+        <Analytics/>
       </body>
     </html>
-    </Analytics>
   );
 };
