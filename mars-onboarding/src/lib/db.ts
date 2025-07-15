6995689d4d379c24c198c7f636f8ce9d296d4486
Mikhail K <108739035+mkornilov07@users.noreply.github.com>
2025-07-15 15:31:49 -0700

    Update db.ts

--- mars-onboarding/src/lib/db.ts
+++ mars-onboarding/src/lib/db.ts
@@ -1,7 +1,7 @@
 import { NeonHTTPAdapter } from "@lucia-auth/adapter-postgresql";
 import { neon } from "@neondatabase/serverless";
 
-export const db = neon(process.env.POSTGRES_URL??"postgres://default:KrBJTbx4OF3e@ep-bitter-frog-a4jtafac-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require")//'ep-bitter-frog-a4jtafac-pooler.us-east-1.aws.neon.tech');
+export const db = neon(process.env.POSTGRES_URL)
 
 db("CREATE TABLE IF NOT EXISTS users(id TEXT NOT NULL PRIMARY KEY, picture TEXT)");
 db(`CREATE TABLE IF NOT EXISTS session (
