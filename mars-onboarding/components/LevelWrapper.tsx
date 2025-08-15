a3e68323c6f339af960d56a9b84108740c28c3aa
Mikhail <mkornilov07@gmail.com>
2025-08-14 17:17:31 -0700

    remove old ros q1

--- mars-onboarding/components/LevelWrapper.tsx
+++ mars-onboarding/components/LevelWrapper.tsx
@@ -324,38 +324,6 @@ else
         
     ],
     "ros": [
-        /*{
-            lesson: (<><p>A node that publishes messages in a topic is called a <b>publisher</b>.
-                A node that listens to a topic and performs actions based on what messages it receives is called a <b>subscriber</b>.</p>
-                <p>Refer to the documentation below:</p>
-                <li><a target = "_blank" href = "https://docs.ros.org/en/melodic/api/rospy/html/rospy.topics.Subscriber-class.html"><u>ROSPy - Subscriber</u></a></li>
-                <li><a target = "_blank" href = "https://docs.ros.org/en/melodic/api/rospy/html/rospy.topics.Publisher-class.html"><u>ROSPy - Publisher</u></a></li>
-                </>),
-            starterCode: `
-import rospy
-from std_msgs.msg import Int64, String
-def main():
-    #Set up a ROS node that receives IR readings from our sensor and determine 
-    #whether the collection bin is full by looking at the distance
-    #from the sensor to the bottom of the bin
-    pub = rospy.Publisher("is_bin_full", String, queue_size=10)
-    def callback(data):
-        #Publish "yes" if the bin is full (closer than 5 in) and "no" if the bin is not full
-        if data < 5:
-            BLANK
-        else:
-            BLANK
-    rospy.init("bin_measurer")
-    #Subscribe to the "ir_readings" topic, which has messages of type Int64
-    rospy.Subscriber(BLANK)
-    rospy.spin()
-
-if __name__ == "__main__":
-    main()
-`,
-        title: <>Subscribers and Publishers</>,
-        correctAnswers: ['pub.publish(String("yes"))', 'pub.publish(String("no"))', '"ir_readings", Int64, callback']
-    } ROS1 Q1 deprecated*/
     { // ROS Q1
             lesson: (<><p>A node that publishes messages in a topic is called a <b>publisher</b>.
                 A node that listens to a topic and performs actions based on what messages it receives is called a <b>subscriber</b>.</p>
